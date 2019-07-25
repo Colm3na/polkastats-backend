47499c0506589f2aba645b0695657cfa1e21c412
mariopino <info@quequiereshacer.es>
2019-07-25 19:08:23 +0200

    Script offline.js

--- offline.js
+++ offline.js
@@ -2,6 +2,15 @@
 // Required imports
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 
+// Connect to MySQL
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "stats",
+  password: "stats",
+  database: 'validators'
+});
+
 // Local Polkadot node
 var wsProviderUrl = 'ws://127.0.0.1:9944';
 
@@ -24,10 +33,28 @@ async function main () {
 
   if (offlineEvents && offlineEvents.length > 0) {
 
-    console.log(JSON.stringify(offlineEvents));
+    //console.log(JSON.stringify(offlineEvents));
+    
+    offlineEvents.forEach((offlineEvent) => {
 
-    offlineEvents.forEach((val) => {
-      
+      // ["5GnNQbHMgBrENud2k3CkbGBB4Z5uNuR6Y1R2z7amXYv8yLMp",2347862,1]
+
+      console.log(`accountId: ${offlineEvent[0]} blocknumber: ${offlineEvent[1]} times: ${offlineEvent[2]}`);
+
+      var sql = 'SELECT id FROM offline WHERE accountId = \'' + offlineEvent[0] + '\' blocknumber = \'' + offlineEvent[1] + '\' AND  times = \'' + offlineEvent[2] + '\';';
+
+      console.log('sql: ' + sql);
+
+      // Search for offline event in db, insert it if not found
+      con.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        
+        console.log('rows: ' + rows);
+
+      });
+
+
+
       //var sql = "INSERT INTO bonded (accountId, timestamp, amount) VALUES ('" + val.accountId + "', UNIX_TIMESTAMP(), '" + val.stakers.total + "');";
       //console.log(sql);
       
