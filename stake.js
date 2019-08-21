40eba21cc0bf5f9775b55f2994e82c8c545c808d
mariopino <info@quequiereshacer.es>
2019-08-21 13:20:50 +0200

    Migrate stake.js to mysql2/promise

--- stake.js
+++ stake.js
@@ -2,6 +2,9 @@
 // Required imports
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 
+// Promise MySQL lib
+const mysql = require('mysql2/promise');
+
 // Local Polkadot node
 var wsProviderUrl = 'ws://127.0.0.1:9944';
 
@@ -15,21 +18,33 @@ async function main () {
   // Create the API and wait until ready
   const api = await ApiPromise.create(provider);
   
-  // Retrieve active validators
-  const [validators] = await Promise.all([
-    api.query.session.validators()
-  ]);
+  // Fetch active validators
+  const validators = await api.query.session.validators()
 
   if (validators && validators.length > 0) {
+
     // Map staking stats to validators
     const validatorStaking = await Promise.all(
       validators.map(authorityId => api.derive.staking.info(authorityId))
     );
-    validatorStaking.forEach((val) => {
-      //console.log(JSON.stringify({ validator: val.accountId, total: formatDot(val.stakers.total) + ' DOT' }));
-      var sql = "INSERT INTO bonded (accountId, timestamp, amount) VALUES ('" + val.accountId + "', UNIX_TIMESTAMP(), '" + val.stakers.total + "');";
-      console.log(sql);
+
+    //
+    // Connect to MySQL database
+    //
+    const conn = await mysql.createConnection({
+      host: "localhost",
+      user: "stats",
+      password: "stats",
+      database: 'validators'
     });
+
+    for (var i = 0; i < validatorStaking.length; i++) {
+
+      //console.log(validatorStaking[i]);
+      var sqlInsert = "INSERT INTO bonded (accountId, timestamp, amount) VALUES ('" + validatorStaking[i].accountId + "', UNIX_TIMESTAMP(), '" + validatorStaking[i].stakers.total + "');";
+      let [rows, fields] = await conn.execute(sqlInsert, [2, 2]);
+
+    }
   }
 }
 
